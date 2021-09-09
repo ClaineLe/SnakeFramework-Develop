5f8595c7f777223bd57f4f1adbcd759dda5e376f
乐开霖 <kailin.le@yingxiong.com>
2021-09-09 23:09:08 +0800

    Refactor SyncUPMMenu.cs

--- Assets/Editor/Menus/SyncUPMMenu.cs
+++ Assets/Editor/Menus/SyncUPMMenu.cs
@@ -13,103 +13,102 @@ namespace com.snake.framework
         {
             private const string UPM_PATH_ROOT = "Assets/UnityPackages";
 
-            [MenuItem("SnakeTools/同步UPM/com.snake.framework.core")]
-            static public void SyncSnakeFramework_Core()
+            [MenuItem("SnakeTools/调试UPM/com.snake.framework.core")]
+            static public void SyncSnakeFramework_Core_Debug()
+            {
+                _SyncSnakeFramework_Core(true);
+            }
+
+            [MenuItem("SnakeTools/调试UPM/com.snake.framework.imp-network")]
+            static public void SyncSnakeFramework_ImpNetWork_Debug()
+            {
+                _SyncSnakeFramework_ImpNetWork(true);
+            }
+
+            [MenuItem("SnakeTools/调试UPM/com.snake.framework.imp-xlua")]
+            static public void SyncSnakeFramework_ImpXLua_Debug()
+            {
+                _SyncSnakeFramework_ImpXLua(true);
+            }
+            [MenuItem("SnakeTools/发布UPM/com.snake.framework.core")]
+            static public void SyncSnakeFramework_Core_Release()
+            {
+                _SyncSnakeFramework_Core(false);
+            }
+
+            [MenuItem("SnakeTools/发布UPM/com.snake.framework.imp-network")]
+            static public void SyncSnakeFramework_ImpNetWork_Release()
+            {
+                _SyncSnakeFramework_ImpNetWork(false);
+            }
+
+            [MenuItem("SnakeTools/发布UPM/com.snake.framework.imp-xlua")]
+            static public void SyncSnakeFramework_ImpXLua_Release()
+            {
+                _SyncSnakeFramework_ImpXLua(false);
+            }
+
+            static private void _SyncSnakeFramework_Core(bool debug)
             {
                 string unityPackageName = "com.snake.framework.core";
                 string repositoriePath = "../SnakeFramework-Core/";
-                string fullPath = UPM_PATH_ROOT + "/" + unityPackageName;
-                _TickVersion(fullPath, 0, 0, 1);
-                System.IO.DirectoryInfo foldInfo = new System.IO.DirectoryInfo(repositoriePath);
-                if (foldInfo.Exists == false)
-                {
-                    Debug.LogError("目录不存在：" + foldInfo.FullName);
-                    return;
-                }
-
-                Utility.Fold.ClearFold(repositoriePath, new string[]
-                {
-                    "\\.git",
-                });
-
-                Utility.Fold.CopyFold(fullPath, repositoriePath);
+                _CopyToGitRepo(unityPackageName, repositoriePath, new[] { "\\.git", }, null, debug);
             }
 
-            [MenuItem("SnakeTools/同步UPM/com.snake.framework.imp-network")]
-            static public void SyncSnakeFramework_ImpNetWork()
+            static private void _SyncSnakeFramework_ImpNetWork(bool debug)
             {
                 string unityPackageName = "com.snake.framework.imp-network";
                 string repositoriePath = "../SnakeFramework-ImpNetWork/";
-                string fullPath = UPM_PATH_ROOT + "/" + unityPackageName;
-                _TickVersion(fullPath, 0, 0, 1);
-                System.IO.DirectoryInfo foldInfo = new System.IO.DirectoryInfo(repositoriePath);
-                if (foldInfo.Exists == false)
-                {
-                    Debug.LogError("目录不存在：" + foldInfo.FullName);
-                    return;
-                }
-
-                Utility.Fold.ClearFold(repositoriePath, new string[]
-                {
-                    "\\.git",
-                });
-
-                Utility.Fold.CopyFold(fullPath, repositoriePath );
-
+                _CopyToGitRepo(unityPackageName, repositoriePath, new[] { "\\.git", }, null, debug);
             }
 
-            [MenuItem("SnakeTools/同步UPM/com.snake.framework.imp-xlua")]
-            static public void SyncSnakeFramework_ImpXLua()
+            static public void _SyncSnakeFramework_ImpXLua(bool debug)
             {
                 string unityPackageName = "com.snake.framework.imp-xlua";
                 string repositoriePath = "../SnakeFramework-ImpXLua/";
+                _CopyToGitRepo(unityPackageName, repositoriePath, new[] { "\\.git", "\\Plugins", "\\XLua" }, new[] { "\\Plugins", "\\XLua" }, debug);
+            }
+
+            static private System.Version _GetVersion(string upPath)
+            {
+                string key = "version";
+                string jsonPath = upPath + "/package.json";
+                string json = System.IO.File.ReadAllText(jsonPath);
+                JObject upmObject = JsonConvert.DeserializeObject<JObject>(json);
+                return System.Version.Parse(upmObject.GetValue(key).Value<string>());
+            }
+
+            static private void _SetVersion(string upPath, int major, int minor, int build)
+            {
+                string key = "version";
+                string jsonPath = upPath + "/package.json";
+                string json = System.IO.File.ReadAllText(jsonPath);
+                JObject upmObject = JsonConvert.DeserializeObject<JObject>(json);
+                System.Version newVersion = new System.Version(major, minor, build);
+                upmObject[key] = newVersion.ToString();
+                json = JsonConvert.SerializeObject(upmObject, Formatting.Indented);
+                System.IO.File.WriteAllText(jsonPath, json, System.Text.Encoding.Default);
+            }
+
+            static private void _CopyToGitRepo(string unityPackageName, string repositoriePath, string[] repoIgnores, string[] copyIgnores, bool debug)
+            {
                 string fullPath = UPM_PATH_ROOT + "/" + unityPackageName;
-                _TickVersion(fullPath, 0, 0, 1);
+                System.Version version = _GetVersion(fullPath);
+                if (debug)
+                    _SetVersion(fullPath, version.Major, version.Minor, version.Build + 1);
+                else
+                    _SetVersion(fullPath, version.Major, version.Minor + 1, 0);
                 System.IO.DirectoryInfo foldInfo = new System.IO.DirectoryInfo(repositoriePath);
                 if (foldInfo.Exists == false)
                 {
                     Debug.LogError("目录不存在：" + foldInfo.FullName);
                     return;
                 }
+                Utility.Fold.ClearFold(repositoriePath, repoIgnores);
+                Utility.Fold.CopyFold(fullPath, repositoriePath, copyIgnores);
 
-                Utility.Fold.ClearFold(repositoriePath, new string[]
-                {
-                    "\\.git","\\Plugins","\\XLua",
-                });
-
-                Utility.Fold.CopyFold(fullPath, repositoriePath, new string[]
-                {
-                    "\\Plugins","\\XLua",
-                });
+                Debug.Log((debug ? "调试" : "发布") + unityPackageName);
             }
-
-            static private System.Version _TickVersion(string upPath, int major, int minor, int build, bool autoSave = true)
-            {
-                string key = "version";
-                string jsonPath = upPath + "/package.json";
-                JObject upmObject = _ReadPackageJson(jsonPath);
-                System.Version oldVersion = System.Version.Parse(upmObject.GetValue(key).Value<string>());
-                System.Version newVersion = new System.Version(
-                    oldVersion.Major + major,
-                    oldVersion.Minor + minor,
-                    oldVersion.Build + build);
-                upmObject[key] = newVersion.ToString();
-                if (autoSave)
-                    _WritePackageJson(jsonPath, upmObject);
-                return newVersion;
-            }
-
-            static private JObject _ReadPackageJson(string jsonPath)
-            {
-                string json = System.IO.File.ReadAllText(jsonPath);
-                return JsonConvert.DeserializeObject<JObject>(json);
-            }
-            static private void _WritePackageJson(string jsonPath, JObject upmObject)
-            {
-                string json = JsonConvert.SerializeObject(upmObject, Formatting.Indented);
-                System.IO.File.WriteAllText(jsonPath, json, System.Text.Encoding.Default);
-            }
-
         }
     }
 }
