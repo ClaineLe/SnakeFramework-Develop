a92e948172ae7903809f26ba14c3b4b52b10cbf5
乐开霖 <kailin.le@yingxiong.com>
2021-09-18 14:17:46 +0800

    add ParserCSV tools

---
+++ Assets/UnityPackages/com.snake.framework.core/Common/Tools/ParserCSV/States/ValueStartState.cs
@@ -0,0 +1,35 @@
+﻿namespace com.snake.framework
+{
+    namespace tools
+    {
+        public class ValueStartState : ParserState
+        {
+            public ValueStartState(ParserStateMachine machine) : base(machine)
+            {
+            }
+
+            public override void EndOfLine()
+            {
+                machine.context.AddValue();
+                machine.context.AddLine();
+                machine.SetState(machine.LineStartState);
+            }
+
+            public override void AnyChar(char ch)
+            {
+                machine.context.AddChar(ch);
+                machine.SetState(machine.ValueState);
+            }
+
+            public override void Comma()
+            {
+                machine.context.AddValue();
+                machine.SetState(machine.ValueStartState);
+            }
+            public override void Quote()
+            {
+                machine.SetState(machine.QuotedValueState);
+            }
+        }
+    }
+}
