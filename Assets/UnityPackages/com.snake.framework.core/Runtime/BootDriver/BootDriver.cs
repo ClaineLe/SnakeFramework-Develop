7b136a398c1f5f4a8fe5168b53af7a3bbea3b150
乐开霖 <kailin.le@yingxiong.com>
2021-09-24 18:59:44 +0800

    增加：框架配置Settting对象

--- Assets/UnityPackages/com.snake.framework.core/Runtime/BootDriver/BootDriver.cs
+++ Assets/UnityPackages/com.snake.framework.core/Runtime/BootDriver/BootDriver.cs
@@ -4,33 +4,32 @@ namespace com.snake.framework
     {
         public class BootDriver
         {
-            public const string RUNTIME_ASSEMBLY = "Assembly-CSharp";
-            public const string CUSTOM_NAMESPACE = "com.snake.framework.custom.runtime";
-            public string CustomAppFacadeClassName = "AppFacadeCostom";
             public IAppFacadeCostom mAppFacadeCostom { get; private set; }
 
             [UnityEngine.RuntimeInitializeOnLoadMethod]
             static public void BootUp()
             {
-                BootDriver bootDriver = new BootDriver();
+                BootDriverSetting bootDriverSetting = UnityEngine.Resources.Load<BootDriverSetting>(typeof(BootDriverSetting).Name);
+                if (bootDriverSetting == null)
+                {
+                    Debuger.ErrorFormat("没有在Resources目录下找到BootDriverSetting.asset，请右键创建一个");
+                    return;
+                }
+                if (bootDriverSetting.Active == false)
+                    return;
+
+                BootDriver bootDriver = new BootDriver(bootDriverSetting);
                 Singleton<AppFacade>.GetInstance().StartUp(bootDriver.mAppFacadeCostom);
             }
-
-            private BootDriver()
-            {
-                this.mAppFacadeCostom = _CreateCostomAppFacade();
-            }
-
-
-            private IAppFacadeCostom _CreateCostomAppFacade()
+            private BootDriver(BootDriverSetting bootDriverSetting)
             {
                 System.Type type = default;
                 System.Reflection.Assembly[] s_Assemblies = Utility.Assembly.GetAssemblies();
                 foreach (System.Reflection.Assembly assembly in s_Assemblies)
                 {
-                    if (assembly.GetName().Name.Equals(RUNTIME_ASSEMBLY))
+                    if (assembly.GetName().Name.Equals(bootDriverSetting.RuntimeAssemblyName))
                     {
-                        type = assembly.GetType(CUSTOM_NAMESPACE + "." + CustomAppFacadeClassName);
+                        type = assembly.GetType(bootDriverSetting.CustomAppFacadeTypeFullName);
                         break;
                     }
                 }
@@ -41,7 +40,7 @@ namespace com.snake.framework
                 object appFacadeCostomObj = System.Activator.CreateInstance(type);
                 if (appFacadeCostomObj == null)
                     throw new System.Exception("没有找到应用门户的自定义实现对象(IAppFacadeCostom)。");
-                return appFacadeCostomObj as IAppFacadeCostom;
+                this.mAppFacadeCostom = appFacadeCostomObj as IAppFacadeCostom;
             }
         }
     }
---
+++ Assets/UnityPackages/com.snake.framework.core/Runtime/BootDriver/BootDriverSetting.cs
@@ -0,0 +1,13 @@
+﻿namespace com.snake.framework
+{
+    namespace runtime
+    {
+        [UnityEngine.CreateAssetMenu()]
+        public class BootDriverSetting : UnityEngine.ScriptableObject
+        {
+            public bool Active = true;
+            public string RuntimeAssemblyName = "Assembly-CSharp";
+            public string CustomAppFacadeTypeFullName = "com.snake.framework.custom.runtime.AppFacadeCostom";
+        }
+    }
+}
