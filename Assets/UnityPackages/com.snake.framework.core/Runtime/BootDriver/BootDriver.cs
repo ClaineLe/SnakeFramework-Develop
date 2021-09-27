c6cf955a4d90b1223a8309e5d70d75c71702fffa
乐开霖 <kailin.le@yingxiong.com>
2021-09-27 10:38:19 +0800

    add BootUpName to BootDriverSetting.cs

--- Assets/UnityPackages/com.snake.framework.core/Runtime/BootDriver/BootDriver.cs
+++ Assets/UnityPackages/com.snake.framework.core/Runtime/BootDriver/BootDriver.cs
@@ -1,3 +1,5 @@
+using UnityEngine.SceneManagement;
+
 namespace com.snake.framework
 {
     namespace runtime
@@ -15,8 +17,33 @@ namespace com.snake.framework
                     Debuger.ErrorFormat("没有在Resources目录下找到BootDriverSetting.asset，请右键创建一个");
                     return;
                 }
+
                 if (bootDriverSetting.Active == false)
+                {
+                    Debuger.ErrorFormat("框架未激活。bootDriverSetting.Active == false");
                     return;
+                }
+
+                if (string.IsNullOrEmpty(bootDriverSetting.BootUpTagName) == true)
+                {
+                    Debuger.ErrorFormat("启动标记Tag不能为空。bootDriverSetting.BootUpTagName：" + bootDriverSetting.BootUpTagName);
+                    return;
+                }
+
+                try
+                {
+                    if (UnityEngine.GameObject.FindWithTag(bootDriverSetting.BootUpTagName) == null)
+                    {
+                        Debuger.ErrorFormat("没有找到Tag标记为：{0} 的GameObject对象，不是游戏启动场景", bootDriverSetting.BootUpTagName);
+                        return;
+                    }
+                }
+                catch (UnityEngine.UnityException unityEx)
+                {
+                    Debuger.Error("没有找到启动Tag:" + bootDriverSetting.BootUpTagName + ".(" + unityEx.Message + ")");
+                    return;
+                }
+
 
                 BootDriver bootDriver = new BootDriver(bootDriverSetting);
                 Singleton<AppFacade>.GetInstance().StartUp(bootDriver.mAppFacadeCostom);
