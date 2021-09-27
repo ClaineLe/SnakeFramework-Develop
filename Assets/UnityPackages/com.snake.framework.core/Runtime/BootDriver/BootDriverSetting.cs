f76183177dd16ada86023a4edc0f20486df0f79f
乐开霖 <kailin.le@yingxiong.com>
2021-09-27 10:42:28 +0800

    增加：框架配置Settting对象

--- Assets/UnityPackages/com.snake.framework.core/Runtime/BootDriver/BootDriverSetting.cs
+++ Assets/UnityPackages/com.snake.framework.core/Runtime/BootDriver/BootDriverSetting.cs
@@ -6,6 +6,7 @@
         public class BootDriverSetting : UnityEngine.ScriptableObject
         {
             public bool Active = true;
+            public string BootUpTagName = "BootUp";
             public string RuntimeAssemblyName = "Assembly-CSharp";
             public string CustomAppFacadeTypeFullName = "com.snake.framework.custom.runtime.AppFacadeCostom";
         }
