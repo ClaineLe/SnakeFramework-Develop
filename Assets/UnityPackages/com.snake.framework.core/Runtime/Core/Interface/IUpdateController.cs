a56e5283e95fe80c470ec2819c668bf762fb6fb5
乐开霖 <kailin.le@yingxiong.com>
2021-10-21 19:17:16 +0800

    【v0.1.16】Added 增加热更控制器接口

---
+++ Assets/UnityPackages/com.snake.framework.core/Runtime/Core/Interface/IUpdateController.cs
@@ -0,0 +1,13 @@
+﻿namespace com.snake.framework
+{
+    namespace runtime
+    {
+        public interface IUpdateController
+        {
+            bool mIsDone { get; }
+            void Enter();
+            void Tick();
+            void Dispose();
+        }
+    }
+}
