e43ffd8e55fbd242685b635a656004649b6c62d6
乐开霖 <kailin.le@yingxiong.com>
2021-10-21 19:17:03 +0800

    【v0.1.16】Added 增加闪屏界面接口

---
+++ Assets/UnityPackages/com.snake.framework.core/Runtime/Core/Interface/ISplashUserInterface.cs
@@ -0,0 +1,12 @@
+﻿namespace com.snake.framework
+{
+    namespace runtime
+    {
+        public interface ISplashUserInterface
+        {
+            bool mIsDone { get; }
+            void Start();
+            void Tick(float progress);
+        }
+    }
+}
