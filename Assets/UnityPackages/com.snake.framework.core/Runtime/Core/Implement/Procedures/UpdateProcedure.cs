977e64c8ab53c03d139768374f7abde4810068a0
乐开霖 <kailin.le@yingxiong.com>
2021-10-21 19:17:34 +0800

    【v0.1.16】Added 增加热更流程实现

---
+++ Assets/UnityPackages/com.snake.framework.core/Runtime/Core/Implement/Procedures/UpdateProcedure.cs
@@ -0,0 +1,39 @@
+﻿namespace com.snake.framework
+{
+    namespace runtime
+    {
+        internal class UpdateProcedure : BaseProcedure
+        {
+            private IUpdateController _updateController;
+
+            protected override void onEnter(ProcedureManager owner, IState<ProcedureManager> fromState, object userData)
+            {
+                base.onEnter(owner, fromState, userData);
+                if (owner.mFramework.mUpdateController == null)
+                {
+                    throw new System.Exception("没有实现热更控制器");
+                }
+                this._updateController = owner.mFramework.mUpdateController;
+                this._updateController?.Enter();
+            }
+
+            protected override void onTick(ProcedureManager owner, int frameCount, float time, float deltaTime, float unscaledTime, float realElapseSeconds)
+            {
+                this._updateController?.Tick();
+                if (this._updateController?.mIsDone == false)
+                    return;
+                owner.SwitchProcedure<PreloadProcedure>();
+            }
+
+            protected override void onExit(ProcedureManager owner, IState<ProcedureManager> toState)
+            {
+                base.onExit(owner, toState);
+                if (this._updateController != null)
+                {
+                    this._updateController.Dispose();
+                    this._updateController = null;
+                }
+            }
+        }
+    }
+}
