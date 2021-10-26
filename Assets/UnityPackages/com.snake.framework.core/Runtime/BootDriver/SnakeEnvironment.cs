8bcad2fce4cdcc487de19047d772ca5cea7b7830
乐开霖 <kailin.le@yingxiong.com>
2021-10-26 22:00:07 +0800

    【v0.2.5】Added 框架环境变量的设计

---
+++ Assets/UnityPackages/com.snake.framework.core/Runtime/BootDriver/SnakeEnvironment.cs
@@ -0,0 +1,11 @@
+﻿namespace com.snake.framework
+{
+    namespace runtime
+    {
+        [UnityEngine.CreateAssetMenu(fileName = "SnakeEnvironment")]
+        public class SnakeEnvironment : UnityEngine.ScriptableObject 
+        {
+
+        }
+    }
+}
