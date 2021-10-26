6e33620f5747cf5e56482cfc7c309b1daed28214
乐开霖 <kailin.le@yingxiong.com>
2021-10-26 17:00:47 +0800

    新增后台下载插件

---
+++ Assets/UnityPackages/BackgroundDownloads/Plugins/iOS/BackgroundDownloadManager.h
@@ -0,0 +1,23 @@
+#import <Foundation/Foundation.h>
+#import "BackgroundDownloadDelegate.h"
+
+@interface BackgroundDownloadManager : NSObject {
+    NSMutableDictionary *pendingDownloadTasks;
+}
+
++ (BackgroundDownloadManager *) instance;
+
+- (long) startDownload:(NSString *) url;
+- (void) cancelDownloadTask:(long) identifier;
+
+- (void) removeDownloadTask:(NSURLSessionTask *) downloadTask;
+
+- (NSURLSessionTask *) getDownloadTask:(long) identifier;
+- (NSURLSessionTask *) getDownloadTaskWithURL:(NSString *) url;
+
+- (void) setDownloadPath:(NSString *) storagePath;
+- (void) moveDownloadToDestination:(NSURL *) tempFileURL downloadedFilename:(NSString *) filename;
+
+- (void) initDownloadSession;
+
+@end
