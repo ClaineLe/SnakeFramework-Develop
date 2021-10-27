7217dc6c54ecc3bba6d808b91ce26b842c501fec
乐开霖 <kailin.le@yingxiong.com>
2021-10-27 16:30:01 +0800

    抽离热更控制器，闪频界面

--- Assets/Scripts/FrameworkCustom/Runtime/FrameworkExt.cs
+++ Assets/Scripts/FrameworkCustom/Runtime/FrameworkExt.cs
@@ -4,84 +4,12 @@ namespace com.snake.framework
 {
     namespace custom.runtime
     {
-        public class SplashUserInterface : ISplashUserInterface
-        {
-            public string Name => throw new System.NotImplementedException();
-
-            public bool mIsDone => throw new System.NotImplementedException();
-
-            public float mProgress => throw new System.NotImplementedException();
-
-            public string mError => throw new System.NotImplementedException();
-
-            public void Dispose()
-            {
-                throw new System.NotImplementedException();
-            }
-
-            public void Enter(object userData = null)
-            {
-                throw new System.NotImplementedException();
-            }
-
-            public void Exit()
-            {
-                throw new System.NotImplementedException();
-            }
-
-            public void Init()
-            {
-                throw new System.NotImplementedException();
-            }
-
-            public void Tick(int frameCount, float time, float deltaTime, float unscaledTime, float realElapseSeconds)
-            {
-                throw new System.NotImplementedException();
-            }
-        }
-
-        public class UpdateController : IUpdateController
-        {
-            public string Name => throw new System.NotImplementedException();
-
-            public bool mIsDone => throw new System.NotImplementedException();
-
-            public float mProgress => throw new System.NotImplementedException();
-
-            public string mError => throw new System.NotImplementedException();
-
-            public void Dispose()
-            {
-                throw new System.NotImplementedException();
-            }
-
-            public void Enter(object userData = null)
-            {
-                throw new System.NotImplementedException();
-            }
-
-            public void Exit()
-            {
-                throw new System.NotImplementedException();
-            }
-
-            public void Init()
-            {
-                throw new System.NotImplementedException();
-            }
-
-            public void Tick(int frameCount, float time, float deltaTime, float unscaledTime, float realElapseSeconds)
-            {
-                throw new System.NotImplementedException();
-            }
-        }
-
         public class FrameworkExt : ISnakeFrameworkExt
         {
             public void Initialization()
             {
-                SnakeFramework.Instance.SetupSplashUserInterface<SplashUserInterface>();
-                SnakeFramework.Instance.SetupUpdateController<UpdateController>();
+                //SnakeFramework.Instance.SetupSplashUserInterface<SplashUserInterface>();
+                //SnakeFramework.Instance.SetupUpdateController<UpdateController>();
 
                 this._regCostomManagers();
                 this._regProcedures();
