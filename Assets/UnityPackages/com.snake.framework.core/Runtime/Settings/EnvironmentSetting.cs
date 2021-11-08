125fb3b79b66bc1a2ccc2587a0b122e1ec8c65b6
ClaineLe <7941324@qq.com>
2021-11-08 15:38:34 +0800

    【v0.2.10】Modify 修改构建配置Header

--- Assets/UnityPackages/com.snake.framework.core/Runtime/Settings/EnvironmentSetting.cs
+++ Assets/UnityPackages/com.snake.framework.core/Runtime/Settings/EnvironmentSetting.cs
@@ -10,8 +10,10 @@ namespace com.snake.framework
             [Header("资源根目录路径")]
             public string mResRootPath;
 
-            [Header("构建配置路径")]
+#if UNITY_EDITOR
+            [Header("[Editor]构建配置路径(BuilderSetting.asset)")]
             public string mBuilderSettingPath;
+#endif
         }
     }
 }
