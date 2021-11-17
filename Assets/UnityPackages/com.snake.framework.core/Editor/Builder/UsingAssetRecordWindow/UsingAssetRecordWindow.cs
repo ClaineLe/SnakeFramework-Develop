b2d4060b030328e2cbdbd9fd15aada025f85b53d
ClaineLe <7941324@qq.com>
2021-11-17 17:17:08 +0800

    【v0.2.17】Added 资源录制编辑器窗口(preview)

---
+++ Assets/UnityPackages/com.snake.framework.core/Editor/Builder/UsingAssetRecordWindow/UsingAssetRecordWindow.cs
@@ -0,0 +1,63 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+using System.Collections.Generic;
+
+namespace com.snake.framework
+{
+    namespace editor
+    {
+        public class UsingAssetRecordWindow : EditorWindow
+        {
+            static private List<string> _usingAssetList;
+            [MenuItem("SnakeTools/资源录制编辑器")]
+            static public void ShowBuilderEditorWindow()
+            {
+                UsingAssetRecordWindow wnd = GetWindow<UsingAssetRecordWindow>();
+                wnd.titleContent = new GUIContent("资源录制编辑器");
+            }
+
+            private void OnEnable()
+            {
+                reset();
+            }
+
+            private void reset()
+            {
+                _usingAssetList = new List<string>();
+            }
+
+            public void CreateGUI()
+            {
+                VisualElement root = rootVisualElement;
+                var visualTree = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>(SnakeEditorUtility.GetPackagesPath() + "/Editor/Builder/UsingAssetRecordWindow/UsingAssetRecordWindow.uxml");
+                if (visualTree == null)
+                {
+                    Debug.LogError("没有找到资源uxml文件.path:" + SnakeEditorUtility.GetPackagesPath() + "/Editor/Builder/UsingAssetRecordWindow/UsingAssetRecordWindow.uxml");
+                    return;
+                }
+                VisualElement labelFromUXML = visualTree.Instantiate();
+                labelFromUXML.Q<IMGUIContainer>().onGUIHandler = OnGUIWithAssetRoleView;
+                root.Add(labelFromUXML);
+            }
+
+            private Vector2 _scrollPosition;
+            private void OnGUIWithAssetRoleView()
+            {
+                using (EditorGUILayout.ScrollViewScope svs = new EditorGUILayout.ScrollViewScope(_scrollPosition, GUILayout.MaxWidth(360)))
+                {
+                    _scrollPosition = svs.scrollPosition;
+                    for (int i = 0; i < _usingAssetList.Count; i++)
+                    {
+                        GUILayout.Label(_usingAssetList[i]);
+                    }
+                }
+            }
+
+            static public void AddUsingAsset(string assetPath)
+            {
+                _usingAssetList.Add(assetPath);
+            }
+        }
+    }
+}
